420191524c00c8e6e6b791469e9effb0bc065804
Omar <omartsadek@gmail.com>
2022-03-27 15:27:09 -0400

    commit tree 84cfcf5e5dae974ae40c18337a585fd86947eec0

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Follow along with the examples here
+function donothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+}
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
+
+function sayHelloToLiz(){
+    console.log("Hello, Liz!");
+}
+sayHelloToLiz();
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+ }
+sayHelloToSamip();
+
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+
+function doSomething(thing) {
+    console.log(thing);
+}
+
+doSomething("anythingbing bong"); 
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Julio", "hello");
+
+function say(greeting, firstName) {
+      console.log("firstName: ", firstName); 
+      console.log("greeting: ", greeting);
+      console.log(`${greeting}, ${firstName}!`);
+  }
+
+function add(x,y) {
+      x+y;
+return  console.log(add(1,2));
+}
